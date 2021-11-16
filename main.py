b6bb283464dca15654750dc785d16465b75c675f
IamAlex44 <>
2021-11-16 11:08:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+#### Describe each datatype below:(4 marks)
+
+## 4         =integere (int)
+## 5.7       =float
+## True      =boolean (bool)
+## Good Luck =string (str)
+
+#### Which datatype would be useful for storing someone's height? (1 mark)
+## Answer float
+
+#### Which datatype would be useful for storing someone's hair colour?(1 mark)
+## Answer 
+# username = input("what is your username? :")
+# print(username)
+
+
+####Create a variable tha will store the users name.(1 mark)
+# name = ("Alessandro")
+# print(name)
+
+
+####Create a print statement that will print the first 4 characters of the person's name.(3 marks)
+# name = ("Alessandro")
+# print(name[ :4])
+
+
+
+####Convert the user's name to all uppercase characters and print the result
+# name = input("What's your username?: ")
+# name1 = str.upper(name)
+# print(name1)
+
+
+
+
+####Find out how many times the letter A occurs in the user's name
+# name =("Alessandro")
+# str.count(name)
+
+
+
+
+
+
+#### Create a conditional statement to ask a user to enter their age. If they are older than 18 they receive a message saying they can enter the competition, if they are under 18, they receive a message saying they cannot enter the competition.
+# statement = int(input("enter your age :"))
+# if statement > 18:
+#  print("they can enter the competition")
+# elif statement <= 18:
+#  print("they cannot enter the competition")
+
+
+#### Create an empty list called squareNumbers (3 marks)
+# squareNumbers = []
+
+#### Square numbers are the solutions to a number being multiplied by itself( example 1 is a square number because 1 X 1 = 1, 4 is a square number because 2 X 2 = 4 ). 
+###Calculate the first 20 square numbers and put them in the list called squareNumbers. (With loop and .append 10 marks, without, Max 6 marks).
+# count = 1
+# while count <21:
+#   num = count*count
+#   squareNumbers.append(num)
+#   count+=1
+
+
+
+####Print your list (1 mark)]
+# mainthings = ["potateos", "fruit", "orange", "pizza"]
+# print(mainthings)
+
+####Create a variable called userSquare that asks the user for their favourite square number
+# userSquare = (2, 3, 4, 5, 6)
+# username = input("what is your favourite number:")
+# if userSquare = (2)
+# print("it is my favourite numeber")
+
+
+
+#### Add this variable to the end of your list called SquareNumbers
+
+### Create a variable called choice. This variable should choose a random element from your list. Print the variable called choice.(3 marks)
+# choice = ["potateos", "fruit", "orange", "pizza"]
+# print(choice)
+
+
+####Create another print statement that prints tha following output: The random square number is: XX, where XX is where the random square number chosen by the computer.(4 marks)
